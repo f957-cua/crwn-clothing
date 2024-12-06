6fadb4c9fad929fb7c0c53c9b22204e218e3e21f
f957-cua <ivanfiliptschik00@gmail.com>
2024-12-06 17:07:44 +0200

    init base structure

---
+++ src/App.js
@@ -0,0 +1,33 @@
+const App = () => {
+  return (
+    <div className="categories-container">
+      <div className="category-item">
+        {/* <img /> */}
+        <h2>Hats</h2>
+        <p>Shop Now</p>
+      </div>
+      <div className="category-item">
+        {/* <img /> */}
+        <h2>Jackets</h2>
+        <p>Shop Now</p>
+      </div>
+      <div className="category-item">
+        {/* <img /> */}
+        <h2>Sneakers</h2>
+        <p>Shop Now</p>
+      </div>
+      <div className="category-item">
+        {/* <img /> */}
+        <h2>Womans</h2>
+        <p>Shop Now</p>
+      </div>
+      <div className="category-item">
+        {/* <img /> */}
+        <h2>Mans</h2>
+        <p>Shop Now</p>
+      </div>
+    </div>
+  );
+};
+
+export default App;
