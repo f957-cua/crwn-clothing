10a629bb826856bbd5ee62740fc5f4689fb7161b
f957-cua <ivanfiliptschik00@gmail.com>
2025-01-27 00:00:13 +0200

    init stripe

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import {
   store,
-  // persistor
 } from "./store/store";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-// import { PersistGate } from "redux-persist/integration/react";
+import {Elements} from "@stripe/react-stripe-js";
+import {stripePromise} from "./utils/stripe/stripe.utils";
 
 import "./index.scss";
 
@@ -15,11 +15,11 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      {/* <PersistGate persistor={persistor}> */}
       <BrowserRouter>
-        <App />
+        <Elements stripe={stripePromise}>
+          <App />
+        </Elements>
       </BrowserRouter>
-      {/* </PersistGate> */}
     </Provider>
   </React.StrictMode>,
 );
